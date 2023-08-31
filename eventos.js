34ea567eef2e569203775ef69c367de774f3c3c1
niezvillar <niezvillar@gmail.com>
2023-08-30 21:30:34 -0300

    Final modificacion

---
+++ eventos.js
@@ -0,0 +1,9 @@
+
+/*function saludar() {
+    alert("Hola!");
+}*/
+
+const botonDiv = document.querySelector('.boton-div');
+botonDiv.addEventListener('click', () => {
+    alert("Hola! Soy el div");
+});
